b6bf9e04fe7bfde91c5e70237e076d1285248985
Ivan Ivanov <aydikstudio@mail.ru>
2023-07-23 21:55:11 +0300

    s

--- src/pages/shipment/index.tsx
+++ src/pages/shipment/index.tsx
@@ -1,37 +1,142 @@
-import { Box, Button, Container, Grid, Paper, Typography } from "@mui/material"
+import { Box, Button, Checkbox, Container, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material"
 import { useSelector } from "react-redux"
 import { Link, useParams } from 'react-router-dom';
 import DonutLargeIcon from '@mui/icons-material/DonutLarge';
 import WidgetsIcon from '@mui/icons-material/Widgets';
 import LocalShippingIcon from '@mui/icons-material/LocalShipping';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { GridContextProvider, GridDropZone, GridItem, swap  } from "react-grid-dnd";
 import { getColorProcent, getProperty } from "../../utils";
 import './index.scss';
+import * as React from "react";
 
-const columns: GridColDef[] = [
-    { field: 'id', headerName: 'Parcel number', width: 200, sortable: false },
-    { field: 'volume', headerName: 'Volume weight', width: 130, sortable: false },
-    { field: 'admission', headerName: 'Admission date', width: 230, sortable: false },
-   
-  ];
-  
-  const rows = [
-    { id: 1, volume: 'Snow', admission: 'Jon' },
-    { id: 2, volume: 'Lannister', admission: 'Cersei' },
-    { id: 3, volume: 'Lannister', admission: 'Jaime' },
-    { id: 4, volume: 'Stark', admission: 'Arya' },
-    { id: 5, volume: 'Targaryen', admission: 'Daenerys' },
-    { id: 6, volume: 'Melisandre', admission: null },
-    { id: 7, volume: 'Clifford', admission: 'Ferrara' },
-    { id: 8, volume: 'Frances', admission: 'Rossini' },
-    { id: 9, volume: 'Roxie', admission: 'Harvey' },
-  ];
+
 
 
 function Shipment() {
+
     let {id } = useParams();
     const shipment = useSelector((state : any) => state.shipments).find((item: any) => item.number == id);
     const procent = shipment.busy_weigh/shipment.weight*100;
+    const [selectedCount, setSelectedCount] = React.useState(0);
+    const [volumeCount, setVolumeCount] = React.useState(0);
+    const [allCheckBox, setAllCheckBox] = React.useState(false);
+const [rows, setRows] = React.useState([
+    {checkbox: false, parcel_number: 159, volume_weight: 6, admission_date: '24/10/2024'},
+    {checkbox: false, parcel_number: 158, volume_weight: 6, admission_date: '24/10/2024'},
+    {checkbox: false, parcel_number: 157, volume_weight: 6, admission_date: '24/10/2024'},
+  ]);
+
+  const [widgets, setWidgets] = React.useState<string[]>([]);
+
+  function handleOnDrag(e: React.DragEvent) {
+    e.dataTransfer.setData("widgetType", [...rows.filter((item) => item.checkbox == true).map((item) => (
+        item.parcel_number
+    ))].join (',') )
+  }
+
+  function handleOnDrop(e: React.DragEvent) {
+    const widgetType = e.dataTransfer.getData("widgetType") as string;
+    setWidgets([...widgets, widgetType])
+  }
+
+
+
+  function handleDragOver(e: React.DragEvent) {
+    e.preventDefault()
+  }
+
+  React.useEffect(() => {
+    setSelectedCount(rows.filter((item) => item.checkbox == true).length)
+    setVolumeCount( rows.filter((item) => item.checkbox == true).reduce(function (acc, obj) { return acc + obj.volume_weight; }, 0))
+  }, [rows])
+  
+
+
+    
+const checkRow = (id: number, checkbox: boolean) => {
+
+    let updatedList = rows.map((item) => 
+        {
+          if (item.parcel_number == id){
+            return {...item, checkbox: !checkbox}; 
+          }
+          return item; 
+        });
+
+     
+        setRows(updatedList);
+     
+}
+
+
+
+
+  
+
+
+       
+
+
+
+
+
+
+
+const showRow = (row: any) => {
+
+    
+
+    if(row.checkbox) {
+        return(
+            <TableRow key={row.parcel_number} draggable onDragStart={(e) => handleOnDrag(e)}>
+            <TableCell><Checkbox  checked={true} onClick={() => checkRow(row.parcel_number, row.checkbox)} /></TableCell>
+            <TableCell align="right">{row.parcel_number}</TableCell>
+            <TableCell align="right">{row.volume_weight}</TableCell>
+            <TableCell align="right">{row.admission_date}</TableCell>
+          </TableRow>
+        )
+    } else {
+        return(
+            <TableRow key={row.parcel_number} onDragStart={(e) => handleOnDrag(e)}>
+            <TableCell><Checkbox checked={false} onClick={() => checkRow(row.parcel_number, row.checkbox)} /></TableCell>
+            <TableCell align="right">{row.parcel_number}</TableCell>
+            <TableCell align="right">{row.volume_weight}</TableCell>
+            <TableCell align="right">{row.admission_date}</TableCell>
+          </TableRow>
+        )
+    }
+
+ 
+}
+
+
+const onSelectedAll = () => {
+   let allcheck = !allCheckBox
+   let updatedList;
+    if(allcheck) {
+         updatedList = rows.map((item) => {
+
+        
+   
+            return {...item, checkbox: true}; 
+        }
+        );
+    } else {
+        updatedList = rows.map((item) => {
+
+        
+   
+            return {...item, checkbox: false}; 
+        }
+        );
+    }
+
+    
+    setRows(updatedList);
+    setAllCheckBox(allcheck)
+}
+
 
     return (
         <Box style={{position: 'relative', top: -100}}>
@@ -166,11 +271,11 @@ function Shipment() {
         </Box>
         <Box style={{display: 'flex', fontSize: 16, marginTop: 5, marginLeft: 10, color: '#b3b3b3'}}>
             <Box style={{marginRight: 5}}>
-                Selected:<span style={{color: '#6015f9'}}>0</span>
+                Selected:<span style={{color: '#6015f9'}}>{selectedCount}</span>
             </Box>
 
             <Box>
-                Weight, kg:<span style={{color: '#6015f9'}}>0</span>
+                Weight, kg:<span style={{color: '#6015f9'}}>{volumeCount}</span>
             </Box>
         </Box>
 
@@ -179,24 +284,39 @@ function Shipment() {
 
         <Box>
         <div style={{ marginTop: 40, height: 800, width: '100%' }}>
-      <DataGrid
 
-        rows={rows}
-        columns={columns}
-        disableColumnFilter={true}
-        disableColumnMenu={true}
-        disableColumnSelector={true}
-        disableDensitySelector={true}
-        disableRowSelectionOnClick={true}
-        disableVirtualization={true}
-        hideFooter={true}
-        checkboxSelection
-        sx={{  '&.MuiDataGrid-row': { marginTop: 1, marginBottom: 1 }, '& .coloured': { textAlign: 'center', color: '#7181AD' },'& .MuiDataGrid-virtualScroller::-webkit-scrollbar': {display: 'none' } }}
-      />
+        <TableContainer component={Paper}>
+      <Table  size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell><Checkbox  onClick={() => onSelectedAll()}/></TableCell>
+            <TableCell align="right">Parcel number</TableCell>
+            <TableCell align="right">Volume weight</TableCell>
+            <TableCell align="right">Admission date</TableCell> 
+          </TableRow>
+        </TableHead>
+        <TableBody>
+ 
+          {rows.map((row) => (
+             showRow(row)
+          ))}
+   
+        </TableBody>
+      </Table>
+    </TableContainer>
+
     </div>
         </Box>
+        {/* <Box style={{border: '1px solid #ff0000', height: '1000px'}} onDrop={handleOnDrop} onDragOver={handleDragOver}>
+            {widgets.map((widget, index) => (
+                <div className="dropped-widget" key={index}>
+                    {widget}
+                </div>
+            ))}
+  </Box> */}
     </Paper>
-  </Grid>
+  </Grid> 
+
 
 </Grid>
          
